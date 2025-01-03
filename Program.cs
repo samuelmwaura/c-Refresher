53a2e8726e94bf58057927353f2f4b51ca6f3eaf
“Samuel” <samuelmwaura081@gmail.com>
2025-01-04 00:51:26 +0300

    [enum] Enums added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿namespace fundamentals;
+﻿
+namespace fundamentals;
 
+public enum ShippingWays
+{
+    airmail = 1,
+    registereAirMail= 2,
+    express = 3
+}
 class Program
 {
     static void Main(string[] args)
@@ -31,9 +38,22 @@ class Program
         C:\\folder1\\folder2";
         Console.WriteLine(path, path2);
 
+        //enums
+        var mailingMethod = shippingWays.registeredAirMail;
+        Console.WriteLine((int)mailingMethod); //outputting the mailing method's respective integer value
+
+        int receivedMethodIdentifier = 1;
+        Console.WriteLine((shippingWays)receivedMethodIdentifier); //change a received integer to the respective shipping way defined in the our app.We cast the received number into the enum
+
+        Console.WriteLine(shippingWays.express.ToString());//Method calls toStrings everytime,no need to call tostrings
+
+        string receivedStringMethod = "airMail";
+        var parsedReceivedStringMethod = (shippingWays)Enum.Parse(typeof(shippingWays),receivedStringMethod);//
+        //first  parse the string to the type of the specified enum then cast the resultant object using the specified enum
+        //parse -  change a string to another type
+        Console.WriteLine(parsedReceivedStringMethod);
 
 
 
-        
     }
 }
---
+++ mailingEnum.cs
@@ -0,0 +1,10 @@
+namespace fundamentals
+{
+    
+public enum shippingWays
+{
+    airMail = 1,
+    registeredAirMail = 2,
+    express = 3
+}
+}
