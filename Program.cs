e670c181828ec0b8ec67a09d689b6a4f87935e87
Ace <ace@Ace-MacBook.local>
2025-01-03 19:24:52 +0300

    Initial setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace fundamentals;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This is for the Lord!");
+        int[] numbers = new int[3] { 89, 276,47,}; // can get rid of int[] and use var and compile knows
+        Console.WriteLine(numbers[2]);
+
+        numbers[2]=  684;
+        Console.WriteLine(numbers[2]);
+        
+    }
+}
